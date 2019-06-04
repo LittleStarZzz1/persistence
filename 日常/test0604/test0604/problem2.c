47ac3f7356efe91f056773027443fda9f814aa2a
LittleStarZzz1 <yx140513@163.com>
2019-06-04 22:31:47 +0800

    模拟实现strcmp和strncmp

---
+++ 日常/test0604/test0604/problem2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+//Ä£ÄâÊµÏÖstrncmp
+int my_strncmp(const char* str1, const char* str2, unsigned int n){
+	int ret;
+	assert(str1);
+	assert(str2);
+	while (!(ret = *(unsigned char*)str1 - *(unsigned char*)str2) && *str2 && --n){
+		str1++;
+		str2++;
+	}
+	if (ret > 0){
+		return 1;
+	}
+	else if (ret < 0){
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+int main(){
+	char str1[] = "abcdef";
+	char str2[] = "abcdfr";
+	int ret;
+	ret = my_strncmp(str1, str2, 4);
+	printf("%d\n", ret);
+	system("pause");
+	return 0;
+}
