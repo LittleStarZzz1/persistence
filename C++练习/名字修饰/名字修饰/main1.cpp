cb37ec768110fa20a09d75af3850bba1b8002e56
LittleStarZzz1 <yx140513@163.com>
2019-10-07 16:35:07 +0800

    名字修饰和extern "C"

---
+++ C++练习/名字修饰/名字修饰/main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int Add(int left, int right);
+double Add(double left, double right);
+
+int main2()
+{
+	//Add(1, 2);
+	//Add(1.0, 2.0);
+
+	system("pause");
+	return 0;
+}
