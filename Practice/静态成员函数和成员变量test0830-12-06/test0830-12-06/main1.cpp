50e4708e012d1f5429ce19ff73471c8423b5fc83
LittleStarZzz1 <yx140513@163.com>
2019-12-06 17:51:21 +0800

    静态成员函数和静态成员变量

---
+++ Practice/静态成员函数和成员变量test0830-12-06/test0830-12-06/main1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class Testop
+{
+public:
+	int m_a;
+	int m_b;
+
+	Testop() :
+		m_a(0),
+		m_b(0)
+	{
+
+	}
+
+	Testop(int a, int b) :
+		m_a(a),
+		m_b(b)
+	{
+
+	}
+
+	void test1(int a, int b)
+	{
+		test2(2, 4);
+	}
+
+	void test2(int a, int b) const
+	{
+		//test1(2, 4);
+	}
+
+	//运算符重载
+	Testop operator + (const Testop & s) const
+	{
+		Testop res;
+
+		res.m_a = m_a + s.m_a;
+		res.m_b = m_b + s.m_b;
+
+		return res;
+	}
+
+	//赋值运算符重载
+	Testop & operator = (const Testop & s)
+	{
+		m_a = s.m_a;
+		m_b = s.m_b;
+
+		return *this;
+	}
+};
+
+int main1()
+{
+	Testop a(3, 5);
+	Testop b(2, 7);
+
+	Testop c;
+	const Testop d;
+
+	c = a + b;
+
+	cout << c.m_a << ' ' << c.m_b << endl;
+
+	system("pause");
+	return 0;
+}
