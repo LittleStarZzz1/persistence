f10325d3a386d78a3859640564e63019fadb0f8a
LittleStarZzz1 <yx140513@163.com>
2019-12-16 16:48:41 +0800

    路径总数问题

---
+++ 1动态规划问题/路径总数问题12-16/路径总数问题12-16/NumPath.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int>> v(m, vector<int>(n, 1));
+		for (int i = 1; i < m; ++i)
+		{
+			for (int j = 1; j < n; ++j)
+			{
+				v[i][j] = v[i - 1][j] + v[i][j - 1];
+			}
+		}
+		return v[m - 1][n - 1];
+	}
+};
+
+int main1()
+{
+
+
+	system("pause");
+	return 0;
+}
