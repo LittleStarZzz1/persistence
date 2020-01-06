de4ed467026f52c7e86abe4f99191a8a07a44b8c
LittleStarZzz1 <yx140513@163.com>
2020-01-06 18:37:40 +0800

    C++Test11-18

---
+++ C++练习/C++Test11-18/C++第三十二节课11-18/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "binarySortTree.h"
+
+using namespace std;
+
+//¶ş²æËÑË÷Ê÷
+
+int main()
+{
+	nj::BinarySortTree<int> BT;
+
+	BT.insert(5);
+	BT.insert(3);
+	BT.insert(8);
+	BT.insert(6);
+	BT.insert(4);
+	BT.insert(7);
+	BT.insert(1);
+	BT.insert(2);
+
+
+	system("pause");
+	return 0;
+}
