6e9e823b9756f316a48f340794bd04e8d87349d5
LittleStarZzz1 <yx140513@163.com>
2020-01-08 09:26:42 +0800

    test

---
+++ 2C++WinterTest/(1)C++winterTestDay1-1-7/(1)C++winterTestDay1-1-7/main3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main3()
+{
+	int i, j, a[2][3];
+
+	//注意越界了
+	//for (i = 0; i <= 6; ++i)
+	//{
+	//	a[i][i] = i;
+	//}
+
+
+
+	system("pause");
+	return 0;
+}
