39ca94784f76adf6b90b4a85f0b3109c099fd7a7
LittleStarZzz1 <yx140513@163.com>
2020-01-10 18:10:32 +0800

    搜索插入位置

---
+++ 2C++WinterTest/(2)C++WinterTestDay2-1-10/(2)C++WinterTestDay2-1-10/main4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//搜索插入位置
+//二分查找
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+		int left = 0;
+		int right = nums.size() - 1;
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (target < nums[mid])
+			{
+				right = mid - 1;
+			}
+			else if (target > nums[mid])
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return left;
+
+	}
+};
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
