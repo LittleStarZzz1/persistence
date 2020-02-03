451cee9648060378227968cf993844f62fd45438
LittleStarZzz1 <yx140513@163.com>
2020-02-03 12:08:16 +0800

    加一

---
+++ 2C++WinterTest/(8)C++WinterTestDay8-2-03/(8)C++WinterTestDay8-2-03/main1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//º”“ª
+class Solution {
+public:
+	vector<int> plusOne(vector<int>& digits) {
+		int len = digits.size() - 1;
+		for (int i = len; i >= 0; --i)
+		{
+			if (digits[i] == 9)
+			{
+				digits[i] = 0;
+			}
+			else
+			{
+				digits[i]++;
+				return digits;
+			}
+		}
+		digits.push_back(0);
+		digits[0] = 1;
+		return digits;
+	}
+};
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
