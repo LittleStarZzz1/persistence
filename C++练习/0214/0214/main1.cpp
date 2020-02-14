7c4ec90f692be8645c6e68a8982f4df45ea85207
LittleStarZzz1 <yx140513@163.com>
2020-02-14 23:26:34 +0800

    test0214

---
+++ C++练习/0214/0214/main1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+using namespace std;
+
+class A{};
+
+template <class T>
+struct unknown_class
+{
+	typedef T return_type;
+};
+
+/****************************************/
+
+template <class unknown_class>
+struct unknown_class_traits
+{
+	typedef typename unknown_class::value_type return_type;
+};
+
+template <class unknown_class>
+typename unknown_class_traits<unknown_class>::return_type
+func(unknown_class u)
+{
+	typedef typename unknown_class_traits<unknown_class>::return_type return_type;
+	return return_type();
+}
+
+int main()
+{
+	vector<int *> a;
+
+	auto b = func(a);
+
+	cout << typeid(b).name() << endl;
+	system("pause");
+	return 0;
+}
