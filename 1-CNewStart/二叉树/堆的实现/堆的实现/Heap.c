711ce793d03c50391ab48846ea5fe49735225e2d
LittleStarZzz1 <yx140513@163.com>
2020-03-04 18:31:36 +0800

    堆的实现

--- 1-CNewStart/二叉树/堆的实现/堆的实现/Heap.c
+++ 1-CNewStart/二叉树/堆的实现/堆的实现/Heap.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "Heap.h"
+
+void Swap(int * a, int * b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 //小堆
 //执行向下调整算法的条件: 左右子树都为堆
@@ -55,9 +63,8 @@ void AdjustUp(int * a, int child)
 	}
 }
 
-Heap * HeapCreate(HpDataType * a, size_t size)
+Heap * HeapCreate(Heap * hp, HpDataType * a, size_t size)
 {
-	Heap * hp = (Heap*)malloc(sizeof(Heap));
 	hp->_a = malloc(sizeof(HpDataType) * size);
 
 	//hp->_a = a; 不行, 只是完成了指针赋值
@@ -71,6 +78,69 @@ Heap * HeapCreate(HpDataType * a, size_t size)
 		AdjustDown(hp->_a, hp->_size, i);
 	}
 	return hp;
+}
+
+//插入
+void HeapPush(Heap * hp, HpDataType x)
+{
+	if (hp->_size == hp->_capacity)
+	{
+		hp->_capacity *= 2;
+		realloc(hp, sizeof(HpDataType) * hp->_capacity);
+	}
+	hp->_a[hp->_size] = x;
+	hp->_size++;
+	AdjustUp(hp->_a, hp->_size - 1);
+}
+
+//删除
+void HeapPop(Heap * hp)
+{
+	Swap(&hp->_a[0], &hp->_a[hp->_size - 1]);
+	hp->_size--;
+
+	AdjustDown(hp->_a, hp->_size, 0);
+}
+
+//获取堆顶数据
+HpDataType HeapTop(Heap * hp)
+{
+	return hp->_a[0];
+}
+
+//判断堆是否为空
+int HeapEmpty(Heap* hp)
+{
+	return hp->_size == 0 ? 1 : 0;
+}
+
+//打印堆中数据
+void HeapPrint(Heap* hp)
+{
+	for (int i = 0; i < hp->_size; ++i)
+	{
+		printf("%d ", hp->_a[i]);
+	}
+	printf("\n");
+}
+
+//topK问题
+// 最大十个数
+void PrintTopK(int* a, int n, int k)
+{
+	//建立一个10个数的小堆
+	Heap hp;
+	HeapCreate(&hp, a, k);
+
+	for (int i = k; i < n; ++i)
+	{
+		if (HeapTop(&hp) < a[i])
+		{
+			HeapPop(&hp);
+			HeapPush(&hp, a[i]);
+		}
+	}
+	HeapPrint(&hp);
 }
 
 //堆排序
@@ -110,6 +180,14 @@ void HeapSort(int * a, size_t size)
 	
 }
 
+//释放空间
+void HeapDestory(Heap * hp)
+{
+	free(hp->_a);
+	hp->_size == hp->_capacity == 0;
+	hp->_a == NULL;
+}
+
 //堆排序的测试
 void testHeapSort()
 {
@@ -118,9 +196,35 @@ void testHeapSort()
 	HeapSort(a, 10);
 }
 
+//topK问题测试
+void TestTopk()
+{
+	int n = 10000;
+	int * a = (int *)malloc(sizeof(int) * n);
+	srand(time(0));
+
+	for (size_t i = 0; i < n; ++i)
+	{
+		a[i] = rand() % 1000000;
+	}
+	//a[15] = 1000000 + 1;
+	//a[1231] = 1000000 + 2;
+	//a[531] = 1000000 + 30;
+	//a[5121] = 1000000 + 4;
+	//a[115] = 1000000 + 5;
+	//a[2335] = 1000000 + 6;
+	//a[9999] = 1000000 + 7;
+	//a[760] = 1000000 + 8;
+	//a[423] = 1000000 + 9;
+	//a[3144] = 1000000 + 10;
+
+	PrintTopK(a, n, 10);
+}
+
 int main()
 {
-	testHeapSort();
+	//testHeapSort();
+	TestTopk();
 
 	system("pause");
 	return 0;
--- 1-CNewStart/二叉树/堆的实现/堆的实现/Heap.h
+++ 1-CNewStart/二叉树/堆的实现/堆的实现/Heap.h
@@ -10,7 +10,7 @@ typedef struct Heap
 }Heap;
 
 //建堆
-Heap * HeapCreate(HpDataType * a, size_t size);
+Heap * HeapCreate(Heap * hp, HpDataType * a, size_t size);
 //插入
 void HeapPush(Heap * hp, HpDataType x);
 //删除
@@ -18,8 +18,24 @@ void HeapPop(Heap * hp);
 //获取堆顶数据
 HpDataType HeapTop(Heap * hp);
 
+//判断堆是否为空
+int HeapEmpty(Heap* hp);
+
+//打印堆中数据
+void HeapPrint(Heap* hp);
+
 //向下调整算法
 void AdjustDown(HpDataType * a, size_t size, int root);
 
+//向上调整算法
+void AdjustUp(int * a, int child);
+
 //堆排序
 void HeapSort(int * a, size_t size);
+
+// topK问题
+// 最大十个数
+void PrintTopK(int* a, int n, int k);
+
+//释放空间
+void HeapDestory(Heap * hp);
