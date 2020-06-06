ec724049afcd0ae8a90aff773c443050c5afde49
LittleStarZzz1 <yx140513@163.com>
2020-06-06 09:35:42 +0800

    test

---
+++ 1-C++New/PracticeDayTest/Day0606/Day0606/main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int m = 0123, n = 123;//m = 0123, 0加在前面表示八进制数
+
+	printf("%o %o", m, n);//123, 173;
+
+
+	system("pause");
+	return 0;
+}
